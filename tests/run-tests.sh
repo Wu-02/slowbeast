be7a8ac1482048191a52cff72f6060f616d99347
Marek Chalupa <mchqwerty@gmail.com>
2019-09-23 17:36:46 +0200

    fix the test-runner

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -5,7 +5,7 @@ FAILED="no"
 
 for T in $(echo $TESTS); do
 	python "$T.py" > "$T.tmp"
-	diff "$T.tmp" "$T.output" || FAILED="yes"; echo "$T failed!" 1>&2
+	diff "$T.tmp" "$T.output" || { FAILED="yes"; echo "$T failed!" 1>&2 ; }
 	rm "$T.tmp"
 done
 
