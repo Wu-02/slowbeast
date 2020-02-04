a34b3bc12a62cd729b72dfdd380a4001932fae3b
Marek Chalupa <mchqwerty@gmail.com>
2020-02-04 10:36:13 +0100

    add a test for fork()

---
+++ tests/c/fork1-true-unreach-call.c
@@ -0,0 +1,26 @@
+extern void __slowbeast_print();
+
+// RUN: clang %s -emit-llvm -g -c -o %t.bc
+// RUN: rm -rf %t-out
+// RUN: sb -out-dir=%t-out  %t.bc &>%t.log
+// RUN: cat %t.log | FileCheck %s
+
+
+int main(void) {
+	int x = nondet_int();
+	int y = nondet_int();
+	if (x > 0) {
+		__slowbeast_print(x, y);
+		if (y > 0) {
+			__slowbeast_print(x, y);
+		} else {
+			__slowbeast_print(x, y);
+		}
+	} else {
+		__slowbeast_print(x, y);
+	}
+
+	// CHECK: Executed paths: 3
+	// CHCEK: Number of forks on branches: 2
+	// CHECK: Found errors: 0
+}
