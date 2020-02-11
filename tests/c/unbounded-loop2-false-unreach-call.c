2bd6b028ae528caa112977e7e0ebf898cd9b1df1
Marek Chalupa <chalupa@fi.muni.cz>
2020-02-11 10:46:10 +0100

    Add tests for k-induction

---
+++ tests/c/unbounded-loop2-false-unreach-call.c
@@ -0,0 +1,28 @@
+#include <assert.h>
+
+// RUN: clang %s -emit-llvm -g -c -o %t.bc
+// RUN: rm -rf %t-out
+// RUN: sb -out-dir=%t-out %opts %t.bc &>%t.log
+// RUN: cat %t.log | FileCheck %s
+
+extern unsigned nondet(void);
+
+int main(void) {
+	int x = nondet();
+	if (x < 0) {
+		return 0;
+	}
+
+	int n = 0;
+	int y = x;
+	int old_diff = x - n;
+	while (x > 0) {
+		--x;
+		++n;
+		assert(x - n == old_diff - 2);
+		//old_diff -= 2;
+	}
+
+	// CHECK: assertion failed!
+	// CHECK: Found errors: 1
+}
---
+++ tests/c/unbounded-loop2-true-unreach-call.c
@@ -0,0 +1,29 @@
+#include <assert.h>
+
+extern unsigned nondet(void);
+
+// REQUIRES: kind
+// RUN: clang %s -emit-llvm -g -c -o %t.bc
+// RUN: rm -rf %t-out
+// RUN: sb -out-dir=%t-out %opts %t.bc &>%t.log
+// RUN: cat %t.log | FileCheck %s
+
+int main(void) {
+	int x = nondet();
+	if (x < 0) {
+		return 0;
+	}
+
+	int n = 0;
+	int y = x;
+	int old_diff = x - n;
+	while (x > 0) {
+		--x;
+		++n;
+		assert(x - n == old_diff - 2);
+		old_diff -= 2;
+	}
+
+	// CHECK-NOT: assertion failed!
+	// CHECK: Found errors: 0
+}
