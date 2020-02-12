02f0069ad978b7c38970db7c8f3a395282cc7f69
Marek Chalupa <mchqwerty@gmail.com>
2020-02-12 14:07:42 +0100

    add a new test for kind

---
+++ tests/c/unbounded2_true-unreach-call.c
@@ -0,0 +1,26 @@
+#include <assert.h>
+
+// REQUIRES: kind
+// RUN: clang %s -emit-llvm -g -c -o %t.bc
+// RUN: rm -rf %t-out
+// RUN: sb -out-dir=%t-out %opts %t.bc &>%t.log
+// RUN: cat %t.log | FileCheck %s
+
+int main(void) {
+	int x = nondet_int();
+	if (x < 0) {
+		return 0;
+	}
+
+	int y = x;
+
+	while (x > 0) {
+		if (x > y) {
+			--x;
+		}
+		assert(x == y);
+	}
+
+	// CHECK-NOT: assertion failed!
+	// CHECK: Found errors: 0
+}
