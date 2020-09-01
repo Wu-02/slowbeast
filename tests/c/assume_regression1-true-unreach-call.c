ae52136478503840e3b8fa57c0094b5765abca2d
Marek Chalupa <mchqwerty@gmail.com>
2020-09-01 15:54:21 +0200

    tests: add a new test
    Reveals a bug in handling assume statement.

---
+++ tests/c/assume_regression1-true-unreach-call.c
@@ -0,0 +1,24 @@
+#include <assert.h>
+
+// REQUIRES: nokind
+// RUN: clang %s -emit-llvm -g -c -o %t.bc
+// RUN: rm -rf %t-out
+// RUN: sb -out-dir=%t-out %opts %t.bc &>%t.log
+// RUN: cat %t.log | FileCheck %s
+
+#define N 5
+#define M 2
+int main(void) {
+        int x = nondet();
+        __VERIFIER_assume(x == 1);
+        int i;
+        for (i = 0; i < N; ++i) {
+                assert((i <= M && x - i == 1) || (i > M && x == i));
+                // CHECK-NOT: assertion failed!
+                // CHECK: Found errors: 0
+                if (i == M) {
+                        --x;
+                }
+                ++x;
+        }
+}
