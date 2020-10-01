e61dac656a676afc141b04e9afcd581edd292887
Marek Chalupa <mchqwerty@gmail.com>
2020-10-02 01:23:24 +0200

    kindse: re-design the main loop
    So that we can more easily execute loops in special subprocedures.

--- slowbeast/kindse/kindse/kindse.py
+++ slowbeast/kindse/kindse/kindse.py
@@ -24,98 +24,51 @@ class KindSymbolicExecutor(BaseKindSE):
             testgen=testgen,
             opts=opts)
 
-        self.InvGen = InvariantGenerator
+        self.readypaths = []
+        self.stalepaths = []
+
         self.genannot = genannot
         self.invpoints = {}
-        self.invgenerators = {}
         self.have_problematic_path = False
-
-    def getInv(self, loc, states):
-        IG = self.invgenerators.setdefault(loc, self.InvGen(self.getProgram(), loc))
-
-        for inv in IG.generate(states):
-            yield inv
 
     def executeLoop(self, loc, states):
         # assert states are inductive on loc
         return execute_loop(self, loc, states)
 
-    def annotateCFG(self, path, states):
-        """
-        Take the executed path and states that are safe and unsafe
-        and derive annotations of CFG
-        """
-        if not self.genannot:  # we should not generate invariants
-            return
-
-        # FIXME: make CFG an attribute of path
-        loc = path.first()
-        assert isinstance(loc, CFG.AnnotatedNode)
-        if not loc in self.invpoints[loc.getCFG()]:
-            return
-
-        dbg_sec(f"Executing loop from {loc.getBBlock().getID()}")
-        self.executeLoop(loc, states)
-        dbg_sec()
-       #loc = path.first()
-       #dbg_sec(f"Trying to generate annotations for {loc.getBBlock().getID()}")
-       #for inv in self.getInv(loc, states):
-       #    dbg(f"Adding {inv} as assumption to the CFG")
-       #    for annot in inv:
-       #        loc.annotationsBefore.append(annot)
-       #dbg_sec()
-
-    def checkPaths(self):
-        newpaths = []
-        has_err = False
-
-        paths = self.paths
-        for path in paths:
-            first_loc = path.first()
-            if self._is_init(first_loc):
-                r, states = self.checkInitialPath(path)
-                if r is Result.UNSAFE:
-                    self.reportfn(f"Error path: {path}", color="RED")
-                    return r, states  # found a real error
-                elif r is Result.SAFE:
-                    self.reportfn(f"Safe (init) path: {path}", color="DARK_GREEN")
-                    continue  # this path is safe
-                elif r is Result.UNKNOWN:
-                    self.have_problematic_path = True
-                    # there is a problem with this path,
-                    # but we can still find an error
-                    # on some different path
-                    # FIXME: keep it in queue so that
-                    # we can rule out this path by
-                    # annotations from other paths?
-                    continue
-                assert r is None, r
-
-            r = self.executePath(path)
-
-            killed = (s for s in r.other if s.wasKilled()) if r.other else None
-            if killed:
+    def check_path(self, path):
+        first_loc = path.first()
+        if self._is_init(first_loc):
+            r, states = self.checkInitialPath(path)
+            if r is Result.UNSAFE:
+                self.reportfn(f"Error path: {path}", color="RED")
+                return r, states  # found a real error
+            elif r is Result.SAFE:
+                self.reportfn(f"Safe (init) path: {path}", color="DARK_GREEN")
+                return None, None  # this path is safe
+            elif r is Result.UNKNOWN:
                 self.have_problematic_path = True
-                for s in killed:
-                    self.report(s)
+                # there is a problem with this path,
+                # but we can still find an error
+                # on some different path
+                # FIXME: keep it in queue so that
+                # we can rule out this path by
+                # annotations from other paths?
+                return None, None
+            assert r is None, r
 
-            self.annotateCFG(path, r)
+        r = self.executePath(path)
 
-            step = self.getOptions().step
-            if r.errors:
-                self.reportfn(f"Possibly error path: {path}", color="ORANGE")
-                has_err = True
-                newpaths += self.extendPath(path,
-                                            steps=step,
-                                            atmost=step != 1,
-                                            stoppoints=self.invpoints[path[0].getCFG()])
-            else:
-                self.reportfn(f"Safe path: {path}", color="DARK_GREEN")
+        killed = (s for s in r.other if s.wasKilled()) if r.other else None
+        if killed:
+            self.have_problematic_path = True
+            for s in killed:
+                self.report(s)
 
-        self.paths = newpaths
-
-        if not has_err:
-            return Result.SAFE, None
+        if r.errors:
+            self.reportfn(f"Possibly error path: {path}", color="ORANGE")
+            return None, r.errors
+        else:
+            self.reportfn(f"Safe path: {path}", color="DARK_GREEN")
 
         return None, None
 
@@ -153,47 +106,62 @@ class KindSymbolicExecutor(BaseKindSE):
 
         return paths
 
-    def run(self, paths=None, maxk=None):
-        dbg("Performing the k-ind algorithm only for the main function",
-            color="ORANGE")
+    def get_path_to_run(self):
+        ready = self.readypaths
+        if not ready:
+            ready = self.stalepaths
+        if ready:
+            return ready.pop()
+        return None
 
+    def is_inv_loc(self, loc):
+        assert isinstance(loc, CFG.AnnotatedNode), loc
+        return loc in self.invpoints[loc.getCFG()]
+
+    def queue_paths(self, paths):
+        is_inv_loc = self.is_inv_loc
+        for p in paths:
+            if is_inv_loc(p.first()):
+                self.stalepaths.append(p)
+            else:
+                self.readypaths.append(p)
+
+    def extend_and_queue_paths(self, path):
+        step = self.getOptions().step
+        newpaths = self.extendPath(path,
+                                   steps=step,
+                                   atmost=step != 1,
+                                   stoppoints=self.invpoints[path[0].getCFG()])
+        self.queue_paths(newpaths)
+       
+    def run(self, paths=None, maxk=None):
         k = 1
 
-        if paths:
-            self.paths = paths
-        else:
-            self.paths = self.initializePaths()
-
-        if len(self.paths) == 0:
-            print_stdout("Found no error state!", color='GREEN')
-            return 0
+        if paths is None:
+            paths = self.initializePaths()
+        self.queue_paths(paths)
 
         while True:
-            print_stdout(f"Starting iteration {k}")
-            dbg("Got {0} paths in queue".format(len(self.paths)))
+            dbg(f"Got {len(self.readypaths)} paths ready and {len(self.stalepaths)} waiting")
 
-            r, states = self.checkPaths()
-            if r is Result.SAFE:
+            path = self.get_path_to_run()
+            if path is None:
                 if self.have_problematic_path:
                     print_stdout("Enumerating paths finished, but a problem was met.", color='ORANGE')
                     return 1
 
-                print_stdout(
-                    "All possible error paths ruled out!",
-                    color="GREEN")
-                print_stdout("Induction step succeeded!", color="GREEN")
+                print_stdout("Enumerating paths done!", color="GREEN")
                 return 0
-            elif r is Result.UNSAFE:
+
+            #if self.is_inv_loc(path.first()):
+            r, states = self.check_path(path)
+            if r is Result.UNSAFE:
                 for s in states:
                     self.report(s)
                 print_stdout("Error found.", color='RED')
                 return 1
-            elif r is Result.UNKNOWN:
-                for s in states:
-                    self.report(s)
-                print_stdout("Hit a problem, giving up.", color='ORANGE')
-                return 1
-            else:
+            elif states: # got error states that may not be real
+                self.extend_and_queue_paths(path)
                 assert r is None
 
             k += 1
