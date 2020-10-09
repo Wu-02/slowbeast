091e34608b7a373acb6859ed32bafb1ef686697a
Marek Chalupa <mchqwerty@gmail.com>
2020-10-09 16:49:34 +0200

    kindse: split off utils.py file

--- slowbeast/kindse/kindse/kindse.py
+++ slowbeast/kindse/kindse/kindse.py
@@ -5,65 +5,14 @@ from slowbeast.kindse.annotatedcfg import AnnotatedCFGPath, CFG
 from slowbeast.kindse.naive.naivekindse import KindSymbolicExecutor as BasicKindSymbolicExecutor
 from slowbeast.kindse.naive.naivekindse import Result, KindSeOptions
 
-from slowbeast.symexe.pathexecutor import InstrsAnnotation, AssumeAnnotation, AssertAnnotation
-from slowbeast.domains.symbolic import NondetLoad
+from slowbeast.symexe.pathexecutor import AssumeAnnotation, AssertAnnotation
 from slowbeast.solvers.solver import getGlobalExprManager, Solver
 
 from . paths import SimpleLoop
-from . annotations import InvariantGenerator, execute_loop, exec_on_loop, get_inv_candidates
+from . annotations import InvariantGenerator, exec_on_loop, get_inv_candidates
 from . kindsebase import KindSymbolicExecutor as BaseKindSE
+from . utils import state_to_annotation, states_to_annotation, unify_annotations
 
-def state_to_annotation(state):
-    EM = state.getExprManager()
-    return AssumeAnnotation(state.getConstraintsObj().asFormula(EM),
-                            {l : l.load for l in state.getNondetLoads()},
-                            EM)
-
-def unify_annotations(annot1, annot2, EM, toassert=False):
-    """ Take two annotations, unify their variables and "or" them together """
-    Ctor = AssertAnnotation if toassert else AssumeAnnotation
-
-    subs1 = annot1.getSubstitutions()
-    subs2 = annot2.getSubstitutions()
-    expr1 = annot1.getExpr()
-    expr2 = annot2.getExpr()
-    if 0 < len(subs2) < len(subs1) or len(subs1) == 0:
-        subs1, subs2 = subs2, subs1
-        expr1, expr2 = expr2, expr1
-
-    if len(subs1) == 0:
-        assert len(subs2) == 0
-        return Ctor(EM.simplify(EM.Or(expr1, expr2)), {}, EM)
-
-    subs = {}
-    col = False
-    for (val, instr) in subs1.items():
-        instr2 = subs2.get(val)
-        if instr2 and instr2 != instr:
-            # collision
-            freshval = EM.freshValue(val.name(), bw=val.getType().getBitWidth())
-            ndload = NondetLoad.fromExpr(freshval, val.load, val.alloc)
-            expr2 = EM.substitute(expr2, (val, ndload))
-            subs[ndload] = instr2
-
-        # always add this one
-        subs[val] = instr
-
-    # add the rest of subs2
-    for (val, instr) in subs2.items():
-        if not subs.get(val):
-            subs[val] = instr
-
-    return Ctor(EM.simplify(EM.Or(expr1, expr2)), subs, EM)
-
-def states_to_annotation(states):
-    a = None
-    for s in states:
-        EM = s.getExprManager()
-        a = unify_annotations(a or AssumeAnnotation(EM.getFalse(), {}, EM),
-                              state_to_annotation(s), EM)
-    return a
- 
 def abstract(r, post):
     print([x for x in get_inv_candidates(r)])
     for inv in get_inv_candidates(r):
---
+++ slowbeast/kindse/kindse/utils.py
@@ -0,0 +1,55 @@
+from slowbeast.symexe.pathexecutor import AssumeAnnotation, AssertAnnotation
+from slowbeast.domains.symbolic import NondetLoad
+
+def state_to_annotation(state):
+    EM = state.getExprManager()
+    return AssumeAnnotation(state.getConstraintsObj().asFormula(EM),
+                            {l : l.load for l in state.getNondetLoads()},
+                            EM)
+
+def unify_annotations(annot1, annot2, EM, toassert=False):
+    """ Take two annotations, unify their variables and "or" them together """
+    Ctor = AssertAnnotation if toassert else AssumeAnnotation
+
+    subs1 = annot1.getSubstitutions()
+    subs2 = annot2.getSubstitutions()
+    expr1 = annot1.getExpr()
+    expr2 = annot2.getExpr()
+    if 0 < len(subs2) < len(subs1) or len(subs1) == 0:
+        subs1, subs2 = subs2, subs1
+        expr1, expr2 = expr2, expr1
+
+    if len(subs1) == 0:
+        assert len(subs2) == 0
+        return Ctor(EM.simplify(EM.Or(expr1, expr2)), {}, EM)
+
+    subs = {}
+    col = False
+    for (val, instr) in subs1.items():
+        instr2 = subs2.get(val)
+        if instr2 and instr2 != instr:
+            # collision
+            freshval = EM.freshValue(val.name(), bw=val.getType().getBitWidth())
+            ndload = NondetLoad.fromExpr(freshval, val.load, val.alloc)
+            expr2 = EM.substitute(expr2, (val, ndload))
+            subs[ndload] = instr2
+
+        # always add this one
+        subs[val] = instr
+
+    # add the rest of subs2
+    for (val, instr) in subs2.items():
+        if not subs.get(val):
+            subs[val] = instr
+
+    return Ctor(EM.simplify(EM.Or(expr1, expr2)), subs, EM)
+
+def states_to_annotation(states):
+    a = None
+    for s in states:
+        EM = s.getExprManager()
+        a = unify_annotations(a or AssumeAnnotation(EM.getFalse(), {}, EM),
+                              state_to_annotation(s), EM)
+    return a
+
+
