c82e4822feb9f2151b9b6881b8210e2ef543df31
Marek Chalupa <mchqwerty@gmail.com>
2020-11-30 21:01:08 +0100

    add a test

---
+++ tests/c/float12.c
@@ -0,0 +1,25 @@
+#include <assert.h>
+
+// RUN: clang %s -emit-llvm -g -c -o %t.bc
+// RUN: rm -rf %t-out
+// RUN: sb -out-dir=%t-out %opts %t.bc &>%t.log
+// RUN: cat %t.log | FileCheck %s
+
+
+
+extern float nondet_float(void);
+extern unsigned char nondet_uchar(void);
+int main()
+{
+  float f = nondet_float();
+  double d;
+  unsigned char x = nondet_uchar();
+
+  d=f;
+  
+  if(f==x)
+    assert(d==x);
+
+	// CHECK-NOT: assertion failed!
+	// CHECK: Found errors: 0
+}
