c42ffe6a8d2b8b20c3c1b1860a08f8af94d7d058
Marek Chalupa <mchqwerty@gmail.com>
2021-03-16 17:27:34 +0100

    BSE: remove unused functions

--- slowbeast/bse/bse.py
+++ slowbeast/bse/bse.py
@@ -27,7 +27,6 @@ def check_paths(executor, paths, pre=None, post=None):
 
     return result
 
-
 class BackwardSymbolicInterpreter(SymbolicInterpreter):
     def __init__(
         self, prog, ohandler=None, opts=KindSEOptions(), programstructure=None
