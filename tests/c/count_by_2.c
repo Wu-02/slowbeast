44e4d8109f35c80ee9131adb354a8137408dcfa2
Marek Chalupa <mchqwerty@gmail.com>
2021-04-08 18:15:19 +0200

    tests: add a new regression test

---
+++ tests/c/count_by_2.c
@@ -0,0 +1,21 @@
+#include <assert.h>
+
+// REQUIRES: unbounded
+//
+// RUN: clang %s -emit-llvm -g -c -o %t.bc
+// RUN: rm -rf %t-out
+// RUN: timeout 30 sb -out-dir=%t-out %opts %t.bc &>%t.log
+// RUN: cat %t.log | FileCheck %s
+
+
+
+int main() {
+    int i;
+    for (i = 0; i < 1000000; i += 2)
+        ;
+    assert(i == 1000000);
+
+	// CHECK-NOT: Error found.
+	// CHECK: Found errors: 0
+    return 0;
+}
