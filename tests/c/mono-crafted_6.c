78410f3c7b89062e15ee5709f8292c9991ef9f12
Marek Chalupa <mchqwerty@gmail.com>
2021-04-08 18:00:00 +0200

    tests: add tests for regressions

---
+++ tests/c/mono-crafted_6.c
@@ -0,0 +1,31 @@
+#include <assert.h>
+
+// REQUIRES: unbounded
+// RUN: clang %s -emit-llvm -g -c -o %t.bc
+// RUN: rm -rf %t-out
+// RUN: timeout 30 sb -out-dir=%t-out %opts %t.bc &>%t.log
+// RUN: cat %t.log | FileCheck %s
+
+int main()
+{
+	int x=0,y=500000,z=0;
+	x=0;
+	while(x<1000000){
+		if(x<500000)
+			x++;
+		else{
+			if(x<750000){
+				x++;
+			}
+			else{
+				x=x+2;
+			}
+			y++;
+		}
+	}
+	assert(x==1000000);
+
+	// CHECK-NOT: Error found.
+	// CHECK: Found errors: 0
+	return 0;
+}
