e9e5c6a672c0e9f0cd33bdd1feda13173f94fdfe
Marek Chalupa <mchqwerty@gmail.com>
2021-04-14 11:25:58 +0200

    tests: add interprocedural tests

---
+++ tests/c/interproc1-false.c
@@ -0,0 +1,21 @@
+#include <assert.h>
+
+// UNSUPPORTED: kind
+// RUN: clang %s -emit-llvm -g -c -o %t.bc
+// RUN: rm -rf %t-out
+// RUN: timeout 30 sb -out-dir=%t-out %opts %t.bc &>%t.log
+// RUN: cat %t.log | FileCheck %s
+
+
+
+void foo(int y) {
+    assert(y != 5);
+}
+
+int main(void) {
+    int x = 5;
+    foo(x);
+
+	// CHECK: Error found.
+	// CHECK: Found errors: 1
+}
