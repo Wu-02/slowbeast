fb92443d653269851c745419a4a04cb70fcffe3b
Marek Chalupa <mchqwerty@gmail.com>
2021-04-16 12:18:46 +0200

    tests: add a test for sequential loops

---
+++ tests/c/seq1.c
@@ -0,0 +1,28 @@
+#include <assert.h>
+
+// RUN: clang %s -emit-llvm -g -c -o %t.bc
+// RUN: rm -rf %t-out
+// RUN: timeout 30 sb -out-dir=%t-out %opts %t.bc &>%t.log
+// RUN: cat %t.log | FileCheck %s
+
+int main(void) {
+    int x = 0;
+    int i = 0;
+
+    while (i < 10) { // L3
+        ++i;
+    }
+
+    // assert((x == i && x >= 10) || (x <= 10 && i == 10));
+    while(x < 10) { // L9
+        ++x;
+        // this we need (we are able to prove it if it is present)
+        //assert(i == 10);
+    }
+
+    assert(x == i);
+
+	// CHECK-NOT: assertion failed!
+	// CHECK-NOT: Error found.
+	// CHECK: Found errors: 0
+}
