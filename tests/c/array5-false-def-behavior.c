ca64cdae175b9bc8a9a568a55eed009d534fb14a
Marek Chalupa <mchqwerty@gmail.com>
2021-04-18 21:05:52 +0200

    tests: fix bself tests

--- tests/c/array5-false-def-behavior.c
+++ tests/c/array5-false-def-behavior.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 
 // UNSUPPORTED: bse
+// UNSUPPORTED: bself
 // RUN: clang %s -emit-llvm -g -c -o %t.bc
 // RUN: rm -rf %t-out
 // RUN: timeout 30 sb -out-dir=%t-out %opts %t.bc &>%t.log
--- tests/c/seq1.c
+++ tests/c/seq1.c
@@ -2,6 +2,7 @@
 
 // UNSUPPORTED: bse
 // UNSUPPORTED: kind
+// UNSUPPORTED: kindse
 // RUN: clang %s -emit-llvm -g -c -o %t.bc
 // RUN: rm -rf %t-out
 // RUN: timeout 30 sb -out-dir=%t-out %opts %t.bc &>%t.log
