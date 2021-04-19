c4a12fb5734faea8350e279d997569e973afc934
Marek Chalupa <mchqwerty@gmail.com>
2021-04-19 09:55:56 +0200

    tests: add a test for th previous commit

---
+++ tests/c/jm2006.c
@@ -0,0 +1,36 @@
+// Source: SV-COMP benchmarks
+// Source: Credited to Anubhav Gupta
+// appears in Ranjit Jhala, Ken McMillan: "A Practical and Complete Approach
+// to Predicate Refinement", TACAS 2006
+
+#include <assert.h>
+
+
+// UNSUPPORTED: se
+// UNSUPPORTED: bse
+// UNSUPPORTED: kind
+// RUN: clang %s -emit-llvm -g -c -o %t.bc
+// RUN: rm -rf %t-out
+// RUN: timeout 30 sb -out-dir=%t-out %opts %t.bc &>%t.log
+// RUN: cat %t.log | FileCheck %s
+
+int main() {
+    int i, j;
+    i = __VERIFIER_nondet_int();
+    j = __VERIFIER_nondet_int();
+    if (!(i >= 0 && j >= 0)) return 0;
+    int x = i;
+    int y = j;
+    while(x != 0) {
+        x--;
+        y--;
+        //assert(x == y || i != j);
+    }
+
+    if (i == j) {
+        assert(y == 0);
+	    // CHECK-NOT: assertion failed!
+	    // CHECK: Found errors: 0
+    }
+    return 0;
+}
