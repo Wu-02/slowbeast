e53548f4adbd8b696f18242ed2f74506d5f9472b
Marek Chalupa <mchqwerty@gmail.com>
2021-04-19 10:06:17 +0200

    adjust tests

--- tests/c/jm2006.c
+++ tests/c/jm2006.c
@@ -6,9 +6,7 @@
 #include <assert.h>
 
 
-// UNSUPPORTED: se
-// UNSUPPORTED: bse
-// UNSUPPORTED: kind
+// REQUIRES: bself
 // RUN: clang %s -emit-llvm -g -c -o %t.bc
 // RUN: rm -rf %t-out
 // RUN: timeout 30 sb -out-dir=%t-out %opts %t.bc &>%t.log
--- tests/c/seq1.c
+++ tests/c/seq1.c
@@ -5,7 +5,7 @@
 // UNSUPPORTED: kindse
 // RUN: clang %s -emit-llvm -g -c -o %t.bc
 // RUN: rm -rf %t-out
-// RUN: timeout 30 sb -out-dir=%t-out %opts %t.bc &>%t.log
+// RUN: timeout 60 sb -out-dir=%t-out %opts %t.bc &>%t.log
 // RUN: cat %t.log | FileCheck %s
 
 int main(void) {
