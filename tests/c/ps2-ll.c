b80a5cd123bb9362ac05cbb4c9e2e5dc97c08321
Marek Chalupa <mchqwerty@gmail.com>
2021-04-19 10:13:07 +0200

    tests: add a new regression test

---
+++ tests/c/ps2-ll.c
@@ -0,0 +1,35 @@
+// source: sv-benchmarks
+#include <assert.h>
+
+// REQUIRES: bself
+// RUN: clang %s -emit-llvm -g -c -o %t.bc
+// RUN: rm -rf %t-out
+// RUN: timeout 30 sb -out-dir=%t-out %opts %t.bc &>%t.log
+// RUN: cat %t.log | FileCheck %s
+
+
+int main() {
+    int k;
+    long long y, x, c;
+    k = __VERIFIER_nondet_int();
+
+    y = 0;
+    x = 0;
+    c = 0;
+
+    while (1) {
+        assert((y * y) - 2 * x + y == 0);
+
+        if (!(c < k))
+            break;
+
+        c = c + 1;
+        y = y + 1;
+        x = y + x;
+    }
+    assert((y*y) - 2*x + y == 0);
+     
+    // CHECK-NOT: assertion failed!
+    // CHECK: Found errors: 0
+    return 0;
+}
