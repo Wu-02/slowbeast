487f09186ef68f5d6b396b428e52c7ec80df9beb
Marek Chalupa <mchqwerty@gmail.com>
2021-04-23 22:15:31 +0200

    tests: add a regression test

---
+++ tests/c/in-de20.c
@@ -0,0 +1,32 @@
+// source: sv-benchmarks
+#include <assert.h>
+
+// REQUIRES: bself
+// RUN: clang %s -emit-llvm -g -c -o %t.bc
+// RUN: rm -rf %t-out
+// RUN: timeout 60 sb -out-dir=%t-out %opts %t.bc &>%t.log
+// RUN: cat %t.log | FileCheck %s
+
+int main()
+{
+  unsigned int n = __VERIFIER_nondet_uint();
+  unsigned int x=n, y=0, z;
+  while(x>0)
+  {
+    x--;
+    y++;
+  }
+
+  z = y;
+  while(z>0)
+  {
+    x++;
+    z--;
+  }
+
+  assert(x==n);
+  // CHECK-NOT: assertion failed!
+  // CHECK-NOT: Error found.
+  // CHECK: Found errors: 0
+  return 0;
+}
