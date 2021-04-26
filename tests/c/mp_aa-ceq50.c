48678e80c2360342401cad6b0b841111e393aeef
Marek Chalupa <mchqwerty@gmail.com>
2021-04-26 23:51:19 +0200

    tests: add  a new test

---
+++ tests/c/mp_aa-ceq50.c
@@ -0,0 +1,26 @@
+#include <assert.h>
+
+// UNSUPPORTED: bse
+// UNSUPPORTED: kind
+// RUN: clang %s -emit-llvm -g -c -o %t.bc
+// RUN: rm -rf %t-out
+// RUN: timeout 100 sb -out-dir=%t-out %opts %t.bc &>%t.log
+// RUN: cat %t.log | FileCheck %s
+
+int main() {
+    int i = 0;
+    int x = 1;
+    while (i < 100) {
+        if (i >= 50) {
+            i += 2;
+        } else {
+            ++i;
+            ++x;
+        }
+        if (i == 1)
+            --x;
+    }
+    assert(x == 50);
+	// CHECK-NOT: assertion failed!
+	// CHECK: Found errors: 0
+}
