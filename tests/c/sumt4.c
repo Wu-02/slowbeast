2b01c229d1029a65b1b2c7efc590f92200f5360b
Marek Chalupa <mchqwerty@gmail.com>
2021-04-27 16:37:43 +0200

    tests sumt4: increase timeout
    This test was on the edge...

--- tests/c/sumt4.c
+++ tests/c/sumt4.c
@@ -4,7 +4,7 @@
 // REQUIRES: bself
 // RUN: clang %s -emit-llvm -g -c -o %t.bc
 // RUN: rm -rf %t-out
-// RUN: timeout 30 sb -out-dir=%t-out %opts %t.bc &>%t.log
+// RUN: timeout 60 sb -out-dir=%t-out %opts %t.bc &>%t.log
 // RUN: cat %t.log | FileCheck %s
 
 
