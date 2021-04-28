3a4826fd820c1970fff7a6b51437a712fb2b3ea8
Marek Chalupa <mchqwerty@gmail.com>
2021-04-28 23:51:58 +0200

    tests: add a new test

--- tests/c/string-false.c
+++ tests/c/string-false.c
@@ -1,27 +1,40 @@
 #include <assert.h>
 
+// UNSUPPORTED: kind
+// UNSUPPORTED: kindse
 // RUN: clang %s -emit-llvm -g -c -o %t.bc
 // RUN: rm -rf %t-out
 // RUN: timeout 30 sb -out-dir=%t-out %opts %t.bc &>%t.log
 // RUN: cat %t.log | FileCheck %s
 
 
+
+#define MAX 2
+
+extern char __VERIFIER_nondet_char();
+
 int main()
 {
-  char string_A[2];
-  unsigned int i, nc_A;
+  unsigned char string_A[MAX], string_B[MAX];
+  int nc_A, nc_B;
   
   string_A[0] = 1;
   string_A[1] = 0;
-
+  string_B[0] = 1;
+  string_B[1] = 0;
+  
   nc_A = 0;
   while(string_A[nc_A]!='\0')
     nc_A++;
 
-  i=0;
-  assert(string_A[i] != 1);
+  nc_B = 0;
+  while(string_B[nc_B]!='\0')
+    nc_B++;
 
+  assert(0 >= nc_A || nc_B < nc_A);
   // CHECK: Error found.
   // CHECK: Found errors: 1
+  //
+  return 0;
 }
 
