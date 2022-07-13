e2d6dc7adacb37685992d69815a2bd4d1c81d55f
Marek Chalupa <mchqwerty@gmail.com>
2022-07-13 08:19:58 +0200

    tests: add a new test

---
+++ tests/c/enh-1.c
@@ -0,0 +1,20 @@
+// REQUIRES: unbounded
+// RUN: rm -rf %t-out
+// RUN: timeout 30 sb -out-dir=%t-out %opts %s &>%t.log
+// RUN: cat %t.log | FileCheck %s
+
+#include <assert.h>
+
+#define N 1000000
+
+int main(void) {
+	int x = 0, i = 0;
+	while (x < N) {
+		if (x < i)
+			--i;
+		++x;
+		i += 2;
+	}
+
+	assert(x == i - 1);
+}
