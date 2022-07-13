6de8c60ea9d0adf875e1c2ea5c23d05e97a04d58
Marek Chalupa <mchqwerty@gmail.com>
2022-07-13 08:22:09 +0200

    tests: add a new test

---
+++ tests/c/enh-2.c
@@ -0,0 +1,22 @@
+// REQUIRES: unbounded
+// RUN: rm -rf %t-out
+// RUN: timeout 30 sb -out-dir=%t-out %opts %s &>%t.log
+// RUN: cat %t.log | FileCheck %s
+
+#include <assert.h>
+
+
+int main(void) {
+	int x = 0, i = 0;
+	int N = nondet();
+	if (N <= x)
+		return 0;
+	while (x < N) {
+		if (x < i)
+			--i;
+		++x;
+		i += 2;
+	}
+
+	assert(x == i - 1);
+}
