7a4b531dcdc5c0a966f2be3ba55baa5a596b56de
Marek Chalupa <mchqwerty@gmail.com>
2022-11-16 13:41:16 +0100

    argument: allow setting name

--- slowbeast/ir/argument.py
+++ slowbeast/ir/argument.py
@@ -7,12 +7,16 @@ class Argument(ProgramElement):
     def __init__(self, ty: Optional[Union[PointerType, BitVecType]]) -> None:
         super().__init__()
         self._type = ty
+        self._name = None
 
     def type(self) -> Union[None, BitVecType, PointerType]:
         return self._type
 
+    def set_name(self, nm) -> None:
+        self._name = nm
+
     def __str__(self) -> str:
-        return f"a{self.get_id()}:{self._type}"
+        return f"a{self._name or self.get_id()}:{self._type}"
 
     def as_value(self) -> str:
-        return f"a{self.get_id()}"
+        return f"a{self._name or self.get_id()}"
