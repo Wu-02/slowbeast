4623f6af5a98b5f7d38a0612321f29682ee80d93
Marek Chalupa <mchqwerty@gmail.com>
2023-04-05 17:39:53 +0200

    Add a docstring to the main module

---
+++ slowbeast/__init__.py
@@ -0,0 +1,12 @@
+"""
+Slowbeast
+
+Slowbeast is a framework and python package for static analysis
+of computer programs. The main focus is on symbolic execution algorithms
+and invariant generation.
+
+The goal of this project is to provide an environment for fast prototyping
+of new algorithms based on symbolic execution. Apart from symbolic execution,
+you can find (sometimes partial) implementations of other types of analyses like
+abstract interpretation.
+"""
